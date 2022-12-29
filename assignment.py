f28c3eda83ce1047ce59f8703eeb4aed7d2a1dbc
vkelson-iu <76700802+vkelson-iu@users.noreply.github.com>
2022-12-29 09:38:38 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,11 +1,3 @@
 def square(x: int) -> int:
   """Add your code here"""
-  
-  
-
-  
-# Test the function
-if __name__ == "__main__":
-  assert(square(9) == 81, "The square is incorrect")
-  print("Test succeeded")
-  
+ 
